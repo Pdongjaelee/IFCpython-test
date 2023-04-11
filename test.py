12ca6829abc3cbc2b2fd8fe7ae149f61604ed873
Pdongjae <kyp4810@gmail.com>
2023-04-11 14:57:56 +0900

    ifcopenshell file open command

--- test.py
+++ test.py
@@ -1,9 +1,35 @@
 import ifcopenshell
-from ifcopenshell.api import run
 
-file = ifcopenshell.open("path/to/your/ifc/file.ifc")
+# 해당 경로에서 ifc 파일 열기(ex)유류고)
+file = ifcopenshell.open('C:/Users/USER\PycharmProjects\pythonProject/6_car_IFC4_Reference_View.ifc')
 
-walls = file.by_type("ifcWall")
+# 모든 벽 유형 가져오기
+for wall_type in file.by_type("IfcWallType") :
+    print("The wall type element is", wall_type)
+    print("The name wall of the wall type", wall_type.Name)
+
+
+# 파일의  ifc 스키마
+# print(file.by_guid('2dHAlBcM9FWQwA427Cfhva'))
+
+#파일의 guid로 데이터 가져오기
+# print(file.by_GlobalId('0pVAcfXILDMA$q2e0Z$q82'))
+
+# ifc guid로 열기
+
+# # 벽의 수 확인
+# wall = file.by_type("IfcWall")
+# print(len(wall))
+
+# 요소의 IFC 클래스가 무엇인지 확인
+# wall = file.by_type("IfcWall")[0]
+# print(wall.is_a())
+
+# 해당 요소 정보 다 가져오기
+# wall = file.by_type("IfcWall")[0]
+# print(wall.get_info())
+
+
 
 
 
